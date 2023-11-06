b67cab0781032b8b12e00e5dc9233bd6e43bdb2a
Jeffreydne <Jeffreydne@gmail.com>
2023-11-06 07:37:56 -0800

    initial js with questionBank array and 1st 3 question objects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,53 @@
 console.log("i am working");
+const questionBank = [
+    {
+        Question: "Which of the following is not considered a semantic element?",
+        ansArr: ["<aside>", "<main>", "<nav>", "div"],
+        correctAns: 3
+    },
+    {
+        Question: `h1 h2 { color: black }\nWhat does the above command do ?`,
+        ansArr: ["Changes the background color of all h1 and h2 elements to black","Changes the background color to black, only for h2 elements that are descendants of an h1","Changes the text color of all h1 and h2 elements to black","Changes the text color to black, only for h2 elements that are descendants of an h1"],
+        correctAns: 2
+    },
+    {
+        Question: `In JavaScript, if you have the following array:\nlet array = ["air", "blue", "car", "photo"]\nWhat is the value or array[2]?`,
+        ansArr: ["undefined","air","blue","car"],
+        correctAns: 3
+    },
+    {
+        Question: ``,
+        ansArr: ["","","",""],
+        correctAns:
+    },
+    {
+        Question: ``,
+        ansArr: ["","","",""],
+        correctAns:
+    },
+    {
+        Question: ``,
+        ansArr: ["","","",""],
+        correctAns:
+    },
+    {
+        Question: ``,
+        ansArr: ["","","",""],
+        correctAns:
+    },
+    {
+        Question: ``,
+        ansArr: ["","","",""],
+        correctAns:
+    },
+    {
+        Question: ``,
+        ansArr: ["","","",""],
+        correctAns:
+    },
+    {
+        Question: ``,
+        ansArr: ["","","",""],
+        correctAns:
+    }
+]
